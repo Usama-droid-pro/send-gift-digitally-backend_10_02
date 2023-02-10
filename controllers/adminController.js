2b5ed2166cfe17d79367e43e766b2078f145b658
Usama <uk5458622@gmail.com>
2023-02-10 10:27:23 +0500

    Modificaitons in admin register and update api ,

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,7 @@
 const adminModel= require("../models/adminModel");
 const bcrypt = require("bcryptjs");
+const fs= require('fs')
+
 
 exports.getAllAdmins= (req,res)=>{
     adminModel.find({},function(err, foundResult){
@@ -118,6 +120,98 @@ exports.updatePassword=async (req,res)=>{
      
 }
 
+exports.updateAdmin = async (req,res)=>{
+    try{
+        const email = req.body.email;
+        const user_name = req.body.user_name;
+        const admin_id= req.body.admin_id;
+
+
+        if(!admin_id){
+            return(
+                res.json({
+                    message: "provide admin id",
+                    status:false
+                })
+            )
+        }
+
+        const result = await adminModel.findOneAndUpdate({_id:admin_id} , {email:email, user_name:user_name} , {new:true});
+
+        if(result){
+            res.json({
+                message: "Admin updated",
+                result:result,
+                status:true
+            })
+        }
+        else{
+            res.json({
+                message: "Could not update",
+                status:false,
+            })
+        }
+    }
+    catch(err){
+        res.json({
+            message: "Error Occurred ",
+            status:false,
+            error:err.message
+        })
+    }
+}
+
+exports.updateAdminImage = async (req,res)=>{
+    try{
+        const admin_id = req.body.admin_id;
+        if(!req.file){
+            return (
+                res.json({
+                    message: "Please provide image for image update",
+                    status:false
+                })
+            )
+        }
+
+        const foundImage = await adminModel.findOne({_id:admin_id});
+        if(foundImage.image){
+            if(fs.existsSync(foundImage.image)){
+                fs.unlinkSync(foundImage.image, (err)=>{
+                    if(!err){
+                        console.log("Deleted")
+                    }else{
+                        return false;
+                    }
+                })
+            } 
+            else{
+                console.log('resourece not found')
+            }
+          
+        }
+
+        const result = await adminModel.findOneAndUpdate({_id:admin_id} , {image:req.file.path} , {new:true});
+        if(result){
+            res.json({
+                message: "Image Updated successfully",
+                result:result
+        })
+        }
+        else{
+            res.json({
+                message: "could not update image",
+                result:null
+            })
+        }
+        
+    }
+    catch(err){
+        res.json({
+            message: "Error occurred",
+            error:err.message
+        })
+    }
+}
 // exports.updateProfile= (req,res)=>{
 //     const userId = req.body.userId;
 //     const name = req.body.name;
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -7,15 +7,21 @@ const jwt = require("jsonwebtoken");
 const auth= require("../middlewares/auth")
 const mongoose=require("mongoose")
 const controller= require("../controllers/adminController")
+const upload = require('../middlewares/adminImageMulter')
+
 
 router.get("/allAdmins" ,controller.getAllAdmins)
 router.get("/specificAdmin/:adminId" , controller.getSpecificAdmin)
 router.delete("/deleteAdmin/:adminId" , controller.deleteAdmin);
 router.put("/updateAdminPassword", controller.updatePassword)
+router.put('/updateAdminImage',upload.single("image"),controller.updateAdminImage);
+router.put('/updateAdmin',controller.updateAdmin);
 
- 
 
-router.post("/register",  async (req, res) => {
+
+
+router.post("/register", upload.single('image'), async (req, res) => {
+  let image;
     const { error } = registerSchema.validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
     //Check if the user is already in the db
@@ -30,11 +36,19 @@ router.post("/register",  async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(req.body.password, salt);
   
+
+    if(req.file){
+      image=req.file.path
+    }
+
+  
     //create new user
     const admin = new adminModel({
       _id:mongoose.Types.ObjectId(),
       email: req.body.email,
       password: hashPassword,
+      image:image,
+      user_name:req.body.user_name
      
      
     });
@@ -46,8 +60,8 @@ router.post("/register",  async (req, res) => {
         _id:savedAdmin._id,
         email:savedAdmin.email,
         password:savedAdmin.password,
-
-
+        image:savedAdmin.image,
+        user_name:savedAdmin.user_name
       })
     } catch (err) {
       res.status(400).send(err);
@@ -93,6 +107,7 @@ router.post("/login",async (req, res) => {
 const registerSchema = Joi.object({
     email: Joi.string().min(6).required().email(),
     password: Joi.string().min(6).required(),
+    user_name:Joi.string()
   });
   
   const loginSchema = Joi.object({
